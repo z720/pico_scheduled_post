c8fae8707938e717c537eebe461fe8b3a4d62410
z720 <seb@z720.net>
2014-04-27 17:52:00 +0200

    Initial Commit

---
+++ Pico_Scheduled_Post.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Exclude any futur post from pages listing...
+ *
+ * @author Sebastien Erard
+ * @link http://z720.net/pico/scheduled-post
+ * @license http://opensource.org/licenses/MIT
+ */
+class Pico_Scheduled_Post {
+
+	protected $now;
+
+	public function get_pages(&$pages, &$current_page, &$prev_page, &$next_page)
+	{
+		$this->now = time();
+		$pages = array_filter($pages, array($this, 'isPublished'));
+	}
+gittag
+	protected function isPublished($page) {
+		if(isset($page['date']) && (strtotime($page['date']) - $this->now) > 0) {
+			return false;
+		}
+		return true;
+	}
+}
